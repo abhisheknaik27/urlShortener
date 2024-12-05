026d04d20becdd4495e2a03eba8066ca8aaab596
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-05 13:03:41 +0530

    yup validation, useFetch hook

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Login from "../components/Login";
-import Signup from "../components/Signup";
+import Login from "../components/authentication/Login";
+import Signup from "../components/authentication/Signup";
 const Auth = () => {
   const [searchParams] = useSearchParams();
   return (

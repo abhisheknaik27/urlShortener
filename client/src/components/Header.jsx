89386d4932f482a03ee85237f71b8eb28121071a
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-07 02:28:23 +0530

    logout implemented

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,43 +11,67 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { LinkIcon, LogOut } from "lucide-react";
+import { UrlState } from "@/context";
+import useFetch from "@/hooks/useFetch";
+import { logout } from "../../db/apiAuth";
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = false;
+  const { user, fetchUser } = UrlState();
+  const { loading, funct: fnLogout } = useFetch(logout);
   return (
-    <nav className="flex justify-between items-center">
-      <Link to="/">
-        <img src="/logo.png" alt="url-shortner-logo" className="h-12 w-12" />
-      </Link>
+    <>
+      <nav className="flex justify-between items-center">
+        <Link to="/">
+          <img src="/logo.png" alt="url-shortner-logo" className="h-12 w-12" />
+        </Link>
 
-      <div>
-        {!user ? (
-          <Button onClick={(auth) => navigate("/auth")}>Login</Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Abhishek Naik</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <LinkIcon className=" h-4 w-4" />
-                My Links
-              </DropdownMenuItem>
-              <DropdownMenuItem className="text-red-500">
-                <LogOut className=" h-4 w-4" />
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+        <div>
+          {!user ? (
+            <Button onClick={(auth) => navigate("/auth")}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage
+                    src={user?.user_metadata?.profile_pic}
+                    className="object-cover"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <LinkIcon className=" h-4 w-4" />
+                  My Links
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-red-500">
+                  <LogOut className=" h-4 w-4" />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        fetchUser();
+                        navigate("/");
+                      });
+                    }}
+                  >
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+      {loading && (
+        <BarLoader width={"100%"} className="mb-2 mt-4" color="#36d7b7" />
+      )}
+    </>
   );
 };
 
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "../components/authentication/Login";
 import Signup from "../components/authentication/Signup";
+import { UrlState } from "@/context";
 const Auth = () => {
   const [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+  const navigate = useNavigate();
+  const { isAuthenticated, loading } = UrlState();
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""} `);
+    }
+  }, [isAuthenticated, loading]);
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">
-        {searchParams.get("createNew")
-          ? "Hold Up... lets Login first"
-          : "Login / Signup"}
+        {longLink ? "Hold Up... lets Login first" : "Login / Signup"}
       </h1>
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">

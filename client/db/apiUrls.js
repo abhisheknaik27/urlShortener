cf8e1e63570080db02aba30aca84b530a45352a5
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-07 16:21:01 +0530

    redirection success

--- client/db/apiUrls.js
+++ client/db/apiUrls.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
@@ -47,4 +48,38 @@ export async function createUrl({ title, longUrl, customUrl, user_id}, qrCode) {
         throw new Error('Unable to create short url');
     }
     return data;
+}
+
+export async function getLongUrl(id) {
+    const { data, error } = await supabase.from('urls').select('id, original_url')
+    .or(`short_url.eq.${id}, custom_url.eq.${id}`)
+    .single();
+
+    if(error){
+        console.log(error.message);
+        throw new Error('Error fetching short link');
+    }
+    return data;
+}
+
+const parser = new UAParser();
+export const storeClicks = async ({ id, originalUrl }) => {
+    try {
+        const res = parser.getResult();
+        const device = res.type || 'desktop';
+
+        const response = await fetch('https://ipapi.co/json');
+        const { city, country_name: country } = await response.json();
+
+        await supabase.from('clicks').insert({
+            url_id: id,
+            city: city,
+            device: device,
+            country: country
+            
+        });
+        window.location.href = originalUrl;
+    } catch (error) {
+        console.error('Error recording click',error);
+    }
 }
--- client/src/pages/RedirectLink.jsx
+++ client/src/pages/RedirectLink.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import useFetch from "@/hooks/useFetch";
+import { getLongUrl, storeClicks } from "../../db/apiUrls";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const RedirectLink = () => {
-  return <div>redirect</div>;
+  const { id } = useParams();
+  const { loading, data, funct } = useFetch(getLongUrl, id);
+
+  const { loading: loadingStats, funct: fnStats } = useFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
+  useEffect(() => {
+    funct();
+  }, []);
+  useEffect(() => {
+    if (!loading && data) {
+      fnStats();
+    }
+  }, [loading]);
+
+  if (loading || loadingStats) {
+    return (
+      <>
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+        <br />
+        Redirecting...
+      </>
+    );
+  }
+  return null;
 };
 
 export default RedirectLink;
